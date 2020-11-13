568cb6ff5cb6352d051a5a8dde9aa08ef7c3dd76
Ramin Amiri <zeuspsn.55555@gmail.com>
2020-11-13 18:23:05 -0500

    Added crouching

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -11,6 +11,7 @@ public class PlayerBehaviour : MonoBehaviour
     public float verticalForce;
     public bool isGrounded;
     public bool isJumping;
+    public bool isCrouching;
     public Transform spawnPoint;
 
     private Rigidbody2D m_rigidBody2D;
@@ -26,7 +27,7 @@ public class PlayerBehaviour : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         _Move();
     }
@@ -35,36 +36,49 @@ public class PlayerBehaviour : MonoBehaviour
     {
         if (isGrounded)
         {
-            if (joystick.Horizontal > joystickHorizontalSensitivity)
+            if (!isJumping && !isCrouching)
             {
-                // move right
-                m_rigidBody2D.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
-                m_spriteRenderer.flipX = false;
-                m_animator.SetInteger("AnimState", 1);
-            }
-            else if (joystick.Horizontal < -joystickHorizontalSensitivity)
-            {
-                // move left
-                m_rigidBody2D.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
-                m_spriteRenderer.flipX = true;
-                m_animator.SetInteger("AnimState", 1);
-            }
-            else if(!isJumping)
-            {
-                m_animator.SetInteger("AnimState", 0);
-            }
+                if (joystick.Horizontal > joystickHorizontalSensitivity)
+                {
+                    // move right
+                    m_rigidBody2D.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
+                    m_spriteRenderer.flipX = false;
+                    m_animator.SetInteger("AnimState", (int)PlayerAnimationType.RUN);
+                }
+                else if (joystick.Horizontal < -joystickHorizontalSensitivity)
+                {
+                    // move left
+                    m_rigidBody2D.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
+                    m_spriteRenderer.flipX = true;
+                    m_animator.SetInteger("AnimState", (int)PlayerAnimationType.RUN);
+                }
+                else
+                {
+                    m_animator.SetInteger("AnimState", (int)PlayerAnimationType.IDLE);
+                }
 
+            }
 
             if ((joystick.Vertical > joystickVerticalSensitivity) && (!isJumping))
             {
                 // jump
-                m_rigidBody2D.AddForce(Vector2.up * verticalForce * Time.deltaTime);
-                m_animator.SetInteger("AnimState", 2);
+                m_rigidBody2D.AddForce(Vector2.up * verticalForce);
+                m_animator.SetInteger("AnimState", (int)PlayerAnimationType.JUMP);
                 isJumping = true;
             }
             else
             {
                 isJumping = false;
+            }
+
+            if ((joystick.Vertical < -joystickVerticalSensitivity) && (!isCrouching))
+            {
+                m_animator.SetInteger("AnimState", (int)PlayerAnimationType.CROUCH);
+                isCrouching = true;
+            }
+            else
+            {
+                isCrouching = false;
             }
         }
 
