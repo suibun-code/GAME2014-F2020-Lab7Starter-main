9b5a34206a02dead8edf75f8789178053dd481f8
Ramin Amiri <zeuspsn.55555@gmail.com>
2020-11-13 18:49:13 -0500

    Opposum enemy moving

---
+++ Assets/_Scripts/OpossumBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpossumBehaviour : MonoBehaviour
+{
+    public float runForce;
+    public Rigidbody2D rigidbody2D;
+    public bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _Move();
+    }
+
+    private void _Move()
+    {
+        rigidbody2D.AddForce(Vector2.left * runForce * Time.deltaTime);
+
+        rigidbody2D.velocity *= 0.90f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platforms"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platforms"))
+        {
+            isGrounded = false;
+        }
+    }
+}
--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -86,12 +86,18 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        isGrounded = true;
+        if (other.gameObject.CompareTag("Platforms"))
+        {
+            isGrounded = true;
+        }
     }
 
     private void OnCollisionExit2D(Collision2D other)
     {
-        isGrounded = false;
+        if (other.gameObject.CompareTag("Platforms"))
+        {
+            isGrounded = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
